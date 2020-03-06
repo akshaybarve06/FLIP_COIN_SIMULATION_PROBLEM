68be8a24adb7d6f525594a264c1de26a675dc2fb
Admin <none@gmail.com>
2020-03-06 16:21:12 +0530

    Added: Doublet Combinations and Percentage

--- flipCoinSimulator.sh
+++ flipCoinSimulator.sh
@@ -6,21 +6,26 @@
 # @Since : 06 March 2020 / Friday
 
 declare -A singletDict
-singletDict=(["Heads"]=0 ["Tails"]=0)
-headCount=0
-tailCount=0
+declare -A doubletDict
+
+singletDict=(["H"]=0 ["T"]=0)
+doubletDict=(["HH"]=0 ["HT"]=0 ["TT"]=0 ["TH"]=0)
+
 noOfFlips=10
+coinOutput=O
+flips=0
 
 function headOrTail()
 {
 	if [[ $((RANDOM%2)) -eq 1 ]]
 	then
-		echo Heads
-		coinOutput="Heads"
+#		echo Heads
+		coinOutput=H
 	else
-		echo Tails
-		coinOutput="Tails"
+#		echo Tails
+		coinOutput=T
 	fi
+#	echo $coinOutput
 }
 
 function singletCombination()
@@ -28,16 +33,50 @@ function singletCombination()
 	for (( i=0; i<$noOfFlips; i++ ))
 	do
 		headOrTail
-		if [[ $coinOutput == Heads ]]
+		if [[ $coinOutput == H ]]
 		then
-			singletDict[Heads]=$((${singletDict[Heads]}+1 ))
-			((headCount++))
+			singletDict[H]=$((${singletDict[H]}+1 ))
 		else
-			singletDict[Tails]=$((${singletDict[Tails]}+1 ))
-			((tailCount++))
+			singletDict[T]=$((${singletDict[T]}+1 ))
 		fi
 	done
-	headPercentage=`expr "scale=2; $headCount * 100 / $noOfFlips" | bc`
-	tailPercentage=`expr "scale=2; $tailCount * 100 / $noOfFlips" | bc`
+	headPercentage=`expr "scale=2; ${singletDict[H]} * 100 / $noOfFlips" | bc`
+	tailPercentage=`expr "scale=2; ${singletDict[T]} * 100 / $noOfFlips" | bc`
 }
-singletCombination
+
+function doubletCombination()
+{
+	while [[ $flips -lt $noOfFlips ]]
+	do
+		headOrTail
+		temp1=$coinOutput
+		headOrTail
+		temp2=$coinOutput
+
+		if [[ $temp1$temp2 == HH ]]
+		then
+			doubletDict[HH]=$((${doubletDict[HH]}+1))
+		elif [[ $temp1$temp2 == HT ]]
+		then
+			doubletDict[HT]=$((${doubletDict[HT]}+1))
+		elif [[ $temp1$temp2 == TT ]]
+		then
+			doubletDict[TT]=$((${doubletDict[TT]}+1))
+		elif [[ $temp1$temp2 == TH ]]
+   	then
+      	doubletDict[TH]=$((${doubletDict[TH]}+1))
+		fi
+		((flips++))
+	done
+
+	HHpercentage=`expr "scale=2; ${doubletDict[HH]} * 100 / $flips" | bc`
+	HTpercentage=`expr "scale=2; ${doubletDict[HT]} * 100 / $flips" | bc`
+	TTpercentage=`expr "scale=2; ${doubletDict[TT]} * 100 / $flips" | bc`
+	THpercentage=`expr "scale=2; ${doubletDict[TH]} * 100 / $flips" | bc`
+}
+
+#singletCombination
+
+doubletCombination
+echo ${doubletDict[@]}
+echo ${!doubletDict[@]}
